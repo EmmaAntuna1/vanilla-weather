1b1057c52dcb2d3403b058c6d79f02c40da6b175
EmmaAntuna1 <88903845+EmmaAntuna1@users.noreply.github.com>
2021-08-24 21:24:29 -0500

    Added the row of forecast

--- app.js
+++ app.js
@@ -20,6 +20,35 @@ function FormatData(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  days.forEach(function(day) {
+    forecastHTML = forecastHTML + `
+                <div class="col-2">
+                  <div class="weather-forecast-date">${day}</div>
+                <img
+                src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">
+                18°</span>
+                <span class="weather-forecast-temperature-min">
+                12°</span> 
+                </div>
+              </div>
+          
+            `;
+            });
+  
+            forecastHTML = forecastHTML + `</div>`;
+            forecastElement.innerHTML = forecastHTML;
+          }    
+
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -85,3 +114,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 search("New York");
+displayForecast();
