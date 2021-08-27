57615c6dd1dfbbf4f318833bee6a5bfdda7959bd
EmmaAntuna1 <88903845+EmmaAntuna1@users.noreply.github.com>
2021-08-26 21:39:24 -0500

    added the lon and lat to javascript

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function FormatData(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -48,7 +48,12 @@ function displayForecast() {
             forecastHTML = forecastHTML + `</div>`;
             forecastElement.innerHTML = forecastHTML;
           }    
-
+function getForecast(coordinates) {
+  let apiKey = "d892feb36e6469e2d73fa57992952a0a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -69,6 +74,8 @@ function displayTemperature(response) {
     dateElement.innerHTML = FormatData(response.data.dt * 1000);
     weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     weatherIconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -114,4 +121,3 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 search("New York");
-displayForecast();
